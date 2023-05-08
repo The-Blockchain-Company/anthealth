06b6fcad4f31001008e64027701ddcf3bb8cb3d6
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-08 13:48:59 -0400

    upd

--- public/api.js
+++ public/api.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'anthealth-anthealth-sw00dvz-sitefiles01-14r15b1w3dhz1.s3.amazonaws.com'
+export const baseUrl = 'https://anthealth-anthealth-sw00dvz-sitefiles01-14r15b1w3dhz1.s3.amazonaws.com'
