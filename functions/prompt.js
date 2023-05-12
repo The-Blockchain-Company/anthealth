312a674470c19dbc9f6632084cad804ce51beb06
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-11 21:25:30 -0400

    lambda proxy headers

--- functions/prompt.js
+++ functions/prompt.js
@@ -48,8 +48,9 @@ exports.handler = async (event) => {
         messages: body.choices?.[0]?.message,
       }),
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "https://api.openai.com",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
       },
     }
   }
